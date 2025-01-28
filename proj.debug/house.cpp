6961c0f1803c007df58cb79642ce4edd6ad78721
jlz0727 <jlgzambrano27@gmail.com>
2025-01-28 02:29:06 +0000

    debug and build

---
+++ proj.debug/house.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << "*";
+        for (int j = 0; j < 2 * n - 3; j++)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ proj.debug/sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "ENTER FIRST NUMBER: ";
+    cin >> num1;
+
+    cout << "ENTER SECOND NUMBER: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+
+    cout << "THE SUM IS: " << sum << endl;
+
+    return 0;
+}
